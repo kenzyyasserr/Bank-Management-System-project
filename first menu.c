57ee0c3d8cd466dab64943cb87b7370809ffab27
mariammohamed098 <maryoma.mohamed5@gmail.com>
2025-12-19 21:04:32 +0200

    Add files via upload

---
+++ first menu.c
@@ -0,0 +1,26 @@
+void first_menu()
+{
+    char choice[10];
+    while(1)
+    {
+        printf("\n--- MENU ---\n");
+        printf("1-LOGIN\n");
+        printf("2-QUIT\n");
+        printf("Choose an option: ");
+
+        if (!fgets(choice, sizeof(choice), stdin)) continue;
+        choice[strcspn(choice, "\n")] = '\0';
+
+        if (!strcmp(choice,"1") || !strcasecmp(choice,"LOGIN"))
+        {
+            printf("You chose LOGIN.\n");
+            login();
+            main_menu();
+        }
+        else if (!strcmp(choice,"2") || !strcasecmp(choice,"QUIT"))
+        {
+            printf("Exiting program.\n");
+            break;
+        }
+    }
+}
