d754e26ef541577777279f54c03c6ad3e13920e3
mariammohamed098 <maryoma.mohamed5@gmail.com>
2025-12-19 17:49:47 +0200

    Add files via upload

---
+++ load acc.c
@@ -0,0 +1,24 @@
+void load_accounts()
+{
+    FILE *accounts=fopen("C:\\Users\\Mariam & Mayar\\Downloads\\accounts.txt","r");
+    if(accounts==NULL)
+    {
+        printf("Error could not open account.txt\n");
+        exit(1);
+    }
+    accCounter=0;
+    while(!feof(accounts))
+    {
+        if (fscanf(accounts, " %10[^,],",acc[accCounter].accountNumber) != 1)
+            break;
+        fscanf(accounts, "%99[^,],",acc[accCounter].name);
+        fscanf(accounts, "%99[^,],",acc[accCounter].email);
+        fscanf(accounts, "%f,",&acc[accCounter].balance);
+        fscanf(accounts, "%11[^,],",acc[accCounter].mobile);
+        fscanf(accounts,"%d-%d,",&acc[accCounter].open.month,&acc[accCounter].open.year);
+        fscanf(accounts,"%19[^,\n]",acc[accCounter].status);
+        fgetc(accounts);
+        accCounter++;
+    }
+    fclose(accounts);
+}5
