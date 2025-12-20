1662b2a29fbf3440e40b771d5a6e64b5ac3f05bf
mariammohamed098 <maryoma.mohamed5@gmail.com>
2025-12-20 16:27:39 +0200

    Implement search_account to display account info
    Replaced the search function with search_account to display account details.

--- search.c
+++ search.c
@@ -1,12 +1,19 @@
-int search(char account_numberin[])
+void search_account()
 {
-    int i;
-    for (i = 0; i < accCounter; i++)
+    int idx=get_account_index();  // call function to get account number
+    if (idx == -1)
     {
-        if (!strcmp(acc[i].accountNumber, account_numberin))
-        {
-            return i;   // index
-        }
+        printf("Account not found!\n");
+        return;
     }
-    return -1;
+
+    printf("Account Number: %s\n", acc[idx].accountNumber);
+    printf("Name          : %s\n", acc[idx].name);
+    printf("Email         : %s\n", acc[idx].email);
+    printf("Mobile        : %s\n", acc[idx].mobile);
+    printf("Balance       : %.2f\n", acc[idx].balance);
+    printf("Status        : %s\n", acc[idx].status);
+    printf("Open Date     : %02d/%d\n", acc[idx].open.month, acc[idx].open.year);
+
+    clean_stdin();
 }
