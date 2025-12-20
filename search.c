0a74a52b15ddf23f28ed4474996b35d8b72e68a2
mariammohamed098 <maryoma.mohamed5@gmail.com>
2025-12-20 23:25:23 +0200

    Refactor search_account function for better output

--- search.c
+++ search.c
@@ -1,6 +1,12 @@
 void search_account()
 {
     int idx=get_account_index();  // call function to get account number
+    const char *months[] =
+    {
+        "January", "February", "March", "April", "May", "June",
+        "July", "August", "September", "October", "November", "December"
+    };
+
     if (idx == -1)
     {
         printf("Account not found!\n");
@@ -9,11 +15,18 @@ void search_account()
 
     printf("Account Number: %s\n", acc[idx].accountNumber);
     printf("Name          : %s\n", acc[idx].name);
-    printf("Email         : %s\n", acc[idx].email);
+    printf("E-mail        : %s\n", acc[idx].email);
+    printf("Balance       : %.2f $\n", acc[idx].balance);
     printf("Mobile        : %s\n", acc[idx].mobile);
-    printf("Balance       : %.2f\n", acc[idx].balance);
-    printf("Status        : %s\n", acc[idx].status);
-    printf("Open Date     : %02d/%d\n", acc[idx].open.month, acc[idx].open.year);
+    printf("Date Opened   : %s %d\n",months[acc[idx].open.month - 1],acc[idx].open.year);
+
+    char status_print[20];
+    strcpy(status_print, acc[idx].status);
+
+    for (int j = 0; status_print[j]; j++)
+        status_print[j] = tolower(status_print[j]);
+    status_print[0] = toupper(status_print[0]);
+    printf("Status        : %s\n", status_print);
 
     clean_stdin();
 }
