d9308608c02ecd3d41caea747064d560c7e66d32
mariammohamed098 <maryoma.mohamed5@gmail.com>
2025-12-20 23:15:50 +0200

    Clean up load_users function
    Removed debug print statement from load_users function.

--- main.c
+++ main.c
@@ -1,13 +1,3 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#define max_users 100
-#define max_input 50
-
-char usernames[max_users][max_input];
-char passwords[max_users][max_input];
-int usersCount=0;
-
 void load_users()
 {
     FILE *users=fopen("C:\\Users\\Mariam & Mayar\\Desktop\\users.txt", "r");
@@ -20,12 +10,13 @@ void load_users()
     usersCount=0;
     while (fscanf(users,"%49s %49s",usernames[usersCount],passwords[usersCount]) == 2)
     {
-        printf("%s %s\n",usernames[usersCount],passwords[usersCount]);
+        // printf("%s %s\n",usernames[usersCount],passwords[usersCount]);
         usersCount++;
     }
 
     fclose(users);
 }
+
 void login()
 {
     char username[max_input];
@@ -34,16 +25,18 @@ void login()
 
     while (!success)
     {
-        printf("Enter username :\n");
+        printf("Enter username : ");
+
+
 
         fgets(username, sizeof(username), stdin);
         username[strcspn(username, "\n")] = '\0';
 
-        printf("Enter password :\n");
+        printf("Enter password : ");
         fgets(password, sizeof(password), stdin);
         password[strcspn(password, "\n")] = '\0';
 
-        for (i=0;i<usersCount;i++)
+        for (i=0; i<usersCount; i++)
         {
             if (strcmp(username,usernames[i])==0 && strcmp(password, passwords[i])==0)
             {
@@ -60,10 +53,3 @@ void login()
     }
 }
 
-
-int main()
-{
-    load_users();
-    login();
-    return 0;
-}
