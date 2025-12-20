92fa30e60d8350c3af5be349ed53415726e32ad4
mariammohamed098 <maryoma.mohamed5@gmail.com>
2025-12-20 23:53:10 +0200

    Refactor report function for clarity and efficiency

--- report.c
+++ report.c
@@ -43,24 +43,3 @@ void report()
     }
 
 }
-void saveAcc()
-{
-
-    FILE *accounts=fopen("C:\\Users\\Mariam & Mayar\\Downloads\\accounts.txt", "w");
-    if (accounts == NULL)
-    {
-        printf("Error could not save.\n");
-        return;
-    }
-
-
-    for (int i = 0; i < accCounter; i++)
-    {
-        fprintf(accounts,"%s,%s,%s,%.2f,%s,%02d-%04d,%s\n",acc[i].accountNumber,acc[i].name,acc[i].email,
-                acc[i].balance,acc[i].mobile,acc[i].open.month,acc[i].open.year,acc[i].status);
-    }
-
-    fclose(accounts);
-
-
-}
